9c017a167572323d5e840462dada54834429044c
liamjosefk <liam.dmgurus@gmail.com>
2024-02-20 16:22:07 -0800

    bug fix

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,9 +1,9 @@
-<h2>New Contact Form Submission: {{ $data->name }}</h2>
+<h2>New Contact Form Submission: {{ $name }}</h2>
 <br>
 
 <strong>Lead details: </strong><br>
-<strong>Name: </strong>{{ $data->name }} <br>
-<strong>Email: </strong>{{ $data->email }} <br>
-<strong>Phone: </strong>{{ $data->phone }} <br>
-<strong>Message: </strong>{{ $data->comment }} <br><br>
-<strong>Heard from: </strong>{{ $data->how_heard }} <br><br>
+<strong>Name: </strong>{{ $name }} <br>
+<strong>Email: </strong>{{ $email }} <br>
+<strong>Phone: </strong>{{ $phone }} <br>
+<strong>Message: </strong>{{ $comment }} <br><br>
+<strong>Heard from: </strong>{{ $how_heard }} <br><br>
